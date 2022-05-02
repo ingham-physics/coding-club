3c8bd128176e442059ca023b3b7ae293c8919293
Brendan Whelan <bwheelz360@gmail.com>
2022-05-02 14:45:27 +1000

    rough draft ready

--- some_optimisation_examples/linear_programming.py
+++ some_optimisation_examples/linear_programming.py
@@ -1,10 +1,33 @@
 from scipy.optimize import linprog
 
-c = [-1, 4]
-A = [[-3, 1], [1, 2]]
-b = [6, 4]
 
-x0_bounds = (None, None)
-x1_bounds = (-3, None)
+def example_1():
+    """
+    This is the code copied out from the simple example explained in this tutorial:
+    https://realpython.com/linear-programming-python/
+    :return:
+    """
+    obj = [-1, -2]
+    #      ─┬  ─┬
+    #       │   └┤ Coefficient for y
+    #       └────┤ Coefficient for x
 
-res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds])
+    lhs_ineq = [[2, 1],  # Red constraint left side
+                [-4, 5],  # Blue constraint left side
+                [1, -2]]  # Yellow constraint left side
+
+    rhs_ineq = [20,  # Red constraint right side
+                10,  # Blue constraint right side
+                2]  # Yellow constraint right side
+
+    bnd = [(0, float("inf")),  # Bounds of x
+           (0, float("inf"))]  # Bounds of y
+
+    opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd,
+                  method="revised simplex")
+    return opt
+
+
+if __name__ == '__main__':
+    results = example_1()
+    print(results)
